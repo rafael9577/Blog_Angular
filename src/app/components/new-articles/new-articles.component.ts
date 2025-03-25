27c5a26054bc2963a87f115cd7724fa0c8d3a160
Rafael Ximenes <rafaelximenesaraujo@outlook.com>
2025-03-25 11:31:58 -0300

    menu-bar complete

---
+++ src/app/components/new-articles/new-articles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-articles',
+  imports: [],
+  templateUrl: './new-articles.component.html',
+  styleUrl: './new-articles.component.css'
+})
+export class NewArticlesComponent {
+
+}
---
+++ src/app/components/old-articles/old-articles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-old-articles',
+  imports: [],
+  templateUrl: './old-articles.component.html',
+  styleUrl: './old-articles.component.css'
+})
+export class OldArticlesComponent {
+
+}
